f65c4aac215d79f1a11ed88f7e50234f33fea652
Felipe Cabral <38699509+fkbral@users.noreply.github.com>
2021-11-23 16:10:42 -0300

    feat: aula 03 - funcionalidade de completar/desmarcar habilidades

--- aula-03/index.js
+++ aula-03/index.js
@@ -50,24 +50,55 @@ newSkillForm.onsubmit = function(event) {
 function renderSkillLi(skill) {
   const li = document.createElement('li')
   const textSpan = document.createElement('span')
-
-  // textSpan.classList.add('done')
-  // textSpan.classList.toggle('done')
-
+  
   textSpan.innerText = skill.name
 
   const removeButton = document.createElement('button')
   removeButton.innerText = 'remover'
 
-  removeButton.onclick = function() {
-    skillsUL.removeChild(li)
-    deleteSkill(skill)
+  const completeButton = document.createElement('button')
+  completeButton.innerText = 'completar'
+
+  if (skill.done) {
+    textSpan.classList.add('done')
+    completeButton.innerText = 'desmarcar'
   }
+
+  removeButton.onclick = () => handleDeleteButton(li, skill)
+  completeButton.onclick = (event) => handleCompleteButton(event, skill, textSpan)
 
   li.append(textSpan)
   li.append(removeButton)
+  li.append(completeButton)
 
   skillsUL.append(li)
+}
+
+function handleDeleteButton(li, skill) {
+  // da mesma forma que querySelector pesquisa filhos de um elemento,
+  // closest pesquisa o pai mais próximo que satisfaz o seletor css
+  // skillsUL.removeChild(event.target.parentElement)
+  // skillsUL.removeChild(event.target.closest('li'))
+  skillsUL.removeChild(li)
+  deleteSkill(skill)
+}
+
+function handleCompleteButton(event, skill, textSpan) {
+  // if (textSpan.classList.contains('done')) {
+    //   textSpan.classList.remove('done')
+    //   return
+    // }
+    // textSpan.classList.add('done')
+    // tudo isto acima pode ser substituído por uma única linha utilizando toggle
+    textSpan.classList.toggle('done')
+
+    event.target.innerText = 
+      textSpan.classList.contains('done')
+      ? 'desmarcar'
+      : 'completar'
+
+    skill.done = !skill.done
+    updateSkill(skill)
 }
 
 function saveSkill(skill) {
@@ -78,5 +109,16 @@ function saveSkill(skill) {
 
 function deleteSkill(skillToDelete) {
   skills = skills.filter(skill => skill.name !== skillToDelete.name)
+  localStorage.setItem('skills', JSON.stringify(skills))
+}
+
+function updateSkill(skillToUpdate) {
+  skills = skills.map(skill => {
+    if (skill.name === skillToUpdate.name) {
+      return skillToUpdate
+    }
+    return skill
+  })
+
   localStorage.setItem('skills', JSON.stringify(skills))
 }
